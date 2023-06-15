36a22889fc2fcc80c74e2e877727828966306598
koyouko <45049985+koyouko@users.noreply.github.com>
2023-06-15 02:41:49 -0400

    v1.1

--- verify_topic_ids.py
+++ verify_topic_ids.py
@@ -1,57 +1,73 @@
 #!/usr/bin/env python3
 
 from kazoo.client import KazooClient
+from pathlib import Path
+import argparse
+import glob
 import csv
+import json
 
 OUTPUT_FILE = "topic_id_mismatch.csv"
 
 def get_topic_id_from_zookeeper(zk_client, topic_name):
-    topic_path = "/brokers/topics/" + topic_name
-    topic_data, _ = zk_client.get(topic_path)
-    topic_id = topic_data["topic_id"]
-    return topic_id
+    topic_path = f"/brokers/topics/{topic_name}"
+    try:
+        topic_data, _ = zk_client.get(topic_path)
+        topic_data_decoded = topic_data.decode("utf-8")
+        topic_info = json.loads(topic_data_decoded)
+        topic_id = topic_info["topic_id"]
+        return topic_id
+    except Exception as e:
+        print(f"Error retrieving topic ID from ZooKeeper for topic: {topic_name}")
+        print(str(e))
+        return None
 
 def check_topic_id_mismatch(log_dir, zk_string):
-    zk_topics = []
     zk_client = KazooClient(hosts=zk_string)
-    zk_client.start()
+    try:
+        zk_client.start()
 
-    with open(OUTPUT_FILE, "w", newline="") as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(["Topic Partition", "Topic ID (Metadata)", "Topic ID (ZooKeeper)"])
+        zk_topics = {}
 
-        for metadata_file in log_dir.glob("logs/*/partition.metadata"):
-            topic_partition = metadata_file.parts[-2]
-            topic_name = topic_partition.rsplit("-", 1)[0]
+        with open(OUTPUT_FILE, "w", newline="") as output_file:
+            writer = csv.writer(output_file)
+            writer.writerow(["Topic Partition", "Topic ID in partition.metadata", "Topic ID in ZooKeeper"])
 
-            with open(metadata_file, "r") as metadata:
-                for line in metadata:
-                    if line.startswith("topic_id"):
-                        topic_id_in_md = line.split()[1]
-                        break
+            for metadata_file in glob.glob(f"{log_dir}/*/partition.metadata"):
+                topic_partition = Path(metadata_file).parts[-2]
+                topic_name = topic_partition.rsplit("-", 1)[0]
+                full_topic_partition = f"{log_dir}/{topic_partition}"
 
-            if topic_name not in zk_topics:
-                zk_topics.append(topic_name)
-                topic_id_in_zk = get_topic_id_from_zookeeper(zk_client, topic_name)
-            else:
-                topic_id_in_zk = zk_topics[zk_topics.index(topic_name) + 1]
+                with open(metadata_file, "r") as metadata:
+                    for line in metadata:
+                        if line.startswith("topic_id"):
+                            topic_id_in_md = line.split()[1]
+                            break
 
-            if topic_id_in_md != topic_id_in_zk:
-                writer.writerow([topic_partition, topic_id_in_md, topic_id_in_zk])
+                if topic_name not in zk_topics:
+                    topic_id_in_zk = get_topic_id_from_zookeeper(zk_client, topic_name)
+                    zk_topics[topic_name] = topic_id_in_zk
+                else:
+                    topic_id_in_zk = zk_topics[topic_name]
 
-    zk_client.stop()
+                if topic_id_in_zk is not None and topic_id_in_md != topic_id_in_zk:
+                    writer.writerow([full_topic_partition, topic_id_in_md, topic_id_in_zk])
+
+    except Exception as e:
+        print("Error occurred while checking topic ID mismatch.")
+        print(str(e))
+
+    finally:
+        zk_client.stop()
 
 if __name__ == "__main__":
-    import argparse
-    from pathlib import Path
-
     parser = argparse.ArgumentParser(description="Check topic ID mismatch between partition metadata and ZooKeeper.")
     parser.add_argument("-l", "--log-dir", required=True, help="Path to Kafka log directory")
     parser.add_argument("-z", "--zk-string", required=True, help="ZooKeeper connection string")
-    
+
     args = parser.parse_args()
 
-    log_dir = Path(args.log_dir)
+    log_dir = args.log_dir
     zk_string = args.zk_string
 
     check_topic_id_mismatch(log_dir, zk_string)
