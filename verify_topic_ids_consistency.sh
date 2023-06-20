e015503f886d1c1b94c6fe2ac91837875860950b
koyouko <45049985+koyouko@users.noreply.github.com>
2023-06-20 13:55:45 -0400

    imp v2

--- verify_topic_ids_consistency.sh
+++ verify_topic_ids_consistency.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 OUTPUT_FILE="topic_id_mismatch.txt"
-declare -A zk_topics
+zk_topics=()
 
-while getopts ":l:z:p:b:" opt; do
+while getopts "l:z:p:b:" opt; do
   case $opt in
     l)
       LOG_DIR=$OPTARG
@@ -28,23 +28,24 @@ while getopts ":l:z:p:b:" opt; do
   esac
 done
 
-if [[ -z $LOG_DIR || -z $ZKSTRING || -z $PARTITION_PREFIX || -z $KAFKABIN ]]; then
+if [ -z "$LOG_DIR" ] || [ -z "$ZKSTRING" ] || [ -z "$PARTITION_PREFIX" ] || [ -z "$KAFKABIN" ]; then
   echo "Usage: $0 -l <path_to_kafka_log_dir> -z <zk_ip_and_port_for_connection> -p <partition_prefix> -b <path_to_kafka_bin>"
   exit 1
 fi
 
 for metadata in "$LOG_DIR"/logs/*/"$PARTITION_PREFIX"*; do
-  topic_partition=$(awk -F'/' '{print $5}' <<< "$metadata")
-  topic_name=$(sed 's/-[0-9]\+$//' <<< "$topic_partition")
+  topic_partition=$(echo "$metadata" | awk -F'/' '{print $5}')
+  topic_name=$(echo "$topic_partition" | sed 's/-[0-9]\+$//')
   topic_id_in_md=$(grep 'topic_id' "$metadata" | awk '{print $2}')
 
-  topic_id_in_zk="${zk_topics[$topic_name]}"
-  if [[ -z $topic_id_in_zk ]]; then
-    topic_id_in_zk=$("$KAFKABIN/zookeeper-shell.sh" "$ZKSTRING" get "/brokers/topics/$topic_name" | grep 'topic_id' | jq -r '.topic_id')
-    zk_topics[$topic_name]=$topic_id_in_zk
-  fi
+  topic_id_in_zk=""
+  for topic_data in $(echo "get /brokers/topics/$topic_name" | "$KAFKABIN/zookeeper-shell.sh" "$ZKSTRING" | grep 'topic_id'); do
+    if [ -z "$topic_id_in_zk" ]; then
+      topic_id_in_zk=$(echo "$topic_data" | jq -r '.topic_id')
+    fi
+  done
 
-  if [[ $topic_id_in_md != $topic_id_in_zk ]]; then
+  if [ "$topic_id_in_md" != "$topic_id_in_zk" ]; then
     echo "Found topic id mismatch for $topic_partition -- in partition metadata: $topic_id_in_md and in zookeeper: $topic_id_in_zk" >> "$OUTPUT_FILE"
   fi
 done
